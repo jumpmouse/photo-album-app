8277ab0efaca27fa501caaee1c06644bfefb55a0
Milos <milosbr@gmail.com>
2020-09-15 01:22:57 +0200

    add template and style to login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,25 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginInputData } from '../core/core.interfaces';
+import { LoginService } from '../core/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private loginService: LoginService, private router: Router) {}
 
-  constructor() { }
+  public profileForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+  });
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public onSubmit(formData: LoginInputData): void {
+        this.loginService
+          .loginUser(formData)
+          .subscribe((loginStatus: boolean) => {
+            // TODO: check login status
+            this.router.navigate(['']);
+          });
   }
-
 }
