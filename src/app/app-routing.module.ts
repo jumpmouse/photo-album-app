3b2a20208c3ded7919fe495e990b1f6c1114b8be
Milos <milosbr@gmail.com>
2020-09-14 22:53:27 +0200

    Add router guard (#3)
    * Add guard to private routes
    
    * add login status logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrivateContentGuard } from './core/private-content.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -7,8 +8,26 @@ import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'login', pathMatch: 'full', component: LoginComponent },
-  { path: 'albums', loadChildren: () => import('./album-list/album-list.module').then(m => m.AlbumListModule) },
-  { path: 'album/:albumId/photos', loadChildren: () => import('./photo-list/photo-list.module').then(m => m.PhotoListModule) },
+  {
+    path: '',
+    canActivate: [PrivateContentGuard],
+    children: [
+      {
+        path: 'albums',
+        loadChildren: () =>
+          import('./album-list/album-list.module').then(
+            (m) => m.AlbumListModule
+          ),
+      },
+      {
+        path: 'album/:albumId/photos',
+        loadChildren: () =>
+          import('./photo-list/photo-list.module').then(
+            (m) => m.PhotoListModule
+          ),
+      },
+    ],
+  },
   { path: '**', component: NotFoundComponent },
 ];
 
