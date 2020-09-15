3fb5d454b1597d20d0418a779d2cdf478101e286
Milos <milosbr@gmail.com>
2020-09-15 16:52:49 +0200

    add logic for data matipulation and storage

--- src/app/core/data-provider.service.ts
+++ src/app/core/data-provider.service.ts
@@ -1,13 +1,46 @@
 import { Injectable } from '@angular/core';
-import { Observable, of as ObservableOf } from 'rxjs';
+import { combineLatest, Observable, of as ObservableOf } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { DataTransformService } from './data-transform.service';
+import { QueryService } from './query.service';
+import { StoreService } from './store.service';
+import { UsersResponse, AlbumsResponse, PhotosResponse } from './core.interfaces';
+
+type PrivateContent = [UsersResponse[], AlbumsResponse[], PhotosResponse[]];
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataProviderService {
-  constructor() {}
+
+  private isDataProcessed = false;
+
+  constructor(
+    private storeService: StoreService,
+    private queryService: QueryService,
+    private dataTransformService: DataTransformService
+  ) {}
 
   public getPrivateContent(): Observable<boolean> {
-    return ObservableOf(true);
+    if (this.isDataProcessed) {
+      return ObservableOf(true);
+    }
+    return combineLatest([
+      this.queryService.getUsers(),
+      this.queryService.getAlbums(),
+      this.queryService.getPhotos(),
+    ]).pipe(
+      map(
+        (privateContent: PrivateContent) => {
+          const {
+            albums,
+            photos,
+          } = this.dataTransformService.transformPrivateData(...privateContent);
+          this.storeService.setAlbums(albums);
+          this.storeService.setPhotos(photos);
+        }
+      ),
+      switchMap(() => ObservableOf(true))
+    );
   }
 }
