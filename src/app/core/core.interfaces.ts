0f4bb47eab69dcc23e1bda3f801fcad14a063b5f
Milos <milosbr@gmail.com>
2020-09-15 13:08:26 +0200

    add interfaces for data

--- src/app/core/core.interfaces.ts
+++ src/app/core/core.interfaces.ts
@@ -8,3 +8,58 @@ export interface UserInfo {
   lastName: string;
   email: string;
 }
+
+export interface UsersResponse {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export interface AlbumsResponse {
+  id: number;
+  userId: number;
+  title: string;
+}
+
+export interface PhotosResponse {
+  id: number;
+  albumId: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+export interface Albums {
+  id: number;
+  userId: number;
+  userName: string;
+  title: string;
+  thumbnailUrl: string;
+}
+
+export interface Photos {
+  id: number;
+  userId: number;
+  albumId: number;
+  title: string;
+  thumbnailUrl: string;
+  url: string;
+}
