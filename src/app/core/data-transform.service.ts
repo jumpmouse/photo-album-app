a13d43a03a2a653993cfcb19d89162aa958062be
Milos <milosbr@gmail.com>
2020-09-15 16:33:23 +0200

    implement data transformation for private content

--- src/app/core/data-transform.service.ts
+++ src/app/core/data-transform.service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@angular/core';
-import { LoginInputData, UserInfo } from './core.interfaces';
+import {
+  AlbumsResponse,
+  LoginInputData,
+  PhotosResponse,
+  UserInfo,
+  UsersResponse,
+  TransformedPrivateData,
+  Photos,
+  Albums,
+  StringList
+} from './core.interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -10,5 +20,60 @@ export class DataTransformService {
   public transformUserData(inputData: LoginInputData): UserInfo {
     const lastName = inputData.fullName.split(' ').pop();
     return { ...inputData, lastName };
+  }
+
+  public transformPrivateData(
+    usersResponse: UsersResponse[],
+    albumsResponse: AlbumsResponse[],
+    photosResponse: PhotosResponse[]
+  ): TransformedPrivateData {
+    const usersNameList: StringList = this.mapUsers(usersResponse);
+    return this.mapAlbumsAndPhotos(
+      albumsResponse,
+      photosResponse,
+      usersNameList
+    );
+  }
+
+  private mapUsers(usersResponse: UsersResponse[]): StringList {
+    // tslint:disable-next-line: prefer-const
+    let users = {};
+    for (const user of usersResponse) {
+      users[user.id] = user.name;
+    }
+    return users;
+  }
+
+  private mapAlbumsAndPhotos(
+    albumsResponse: AlbumsResponse[],
+    photosResponse: PhotosResponse[],
+    usersNameList: StringList
+  ): TransformedPrivateData {
+    const transformedData = {
+      albums: [],
+      photos: {}
+    };
+    const allPhotos = [...photosResponse];
+
+    for (const album of albumsResponse) {
+      // tslint:disable-next-line: prefer-const
+      let singleAlbum: Albums = { ...album };
+      singleAlbum.userName = usersNameList[singleAlbum.userId];
+
+      // tslint:disable-next-line: prefer-const
+      const albumPhotos: Photos[] = allPhotos.filter(
+        (photoObject: PhotosResponse, i: number) => {
+          if (photoObject.albumId !== singleAlbum.id) {
+            allPhotos.splice(i, 1);
+            return false;
+          }
+          return true;
+        }
+      );
+      transformedData.photos[singleAlbum.id] = albumPhotos;
+      singleAlbum.thumbnailUrl = albumPhotos[0].thumbnailUrl;
+      transformedData.albums.push(singleAlbum);
+    }
+    return transformedData;
   }
 }
