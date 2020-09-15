416a964759f42f98142457d3c863259811988667
Milos <milosbr@gmail.com>
2020-09-15 02:35:48 +0200

    small fixes

--- src/app/core/store.service.ts
+++ src/app/core/store.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { LoginInputData, UserInfo } from './core.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StoreService {
-  private userInfo: UserInfo;
+  private userInfo$: BehaviorSubject<UserInfo> = new BehaviorSubject(null);
 
   constructor() {}
 
-  public getUserInfo(): UserInfo {
-    return this.userInfo;
+  public get userInfo(): Observable<UserInfo> {
+    return this.userInfo$.asObservable();
   }
 
   public setUserInfo(inputData: LoginInputData): boolean {
     const lastName = inputData.fullName.split(' ').pop();
-    this.userInfo = { ...inputData, lastName};
+    const newUserInfo = { ...inputData, lastName};
+    this.userInfo$.next(newUserInfo);
     return true;
   }
 }
