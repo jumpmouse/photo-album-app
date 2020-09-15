ad14077c63b215a522d6135a725bb6306c6d7d07
Milos <milosbr@gmail.com>
2020-09-15 17:50:49 +0200

    fetch data from server

--- src/app/core/query.service.ts
+++ src/app/core/query.service.ts
@@ -1,20 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of as ObservableOf } from 'rxjs';
-import { AlbumsResponse, PhotosResponse, UsersResponse } from './core.interfaces';
+import { Observable } from 'rxjs';
+import {
+  AlbumsResponse,
+  PhotosResponse,
+  UsersResponse
+} from './core.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QueryService {
-  constructor() {}
+  private urlObject = {
+    users: 'https://jsonplaceholder.typicode.com/users',
+    albums: 'https://jsonplaceholder.typicode.com/albums',
+    photos: 'https://jsonplaceholder.typicode.com/photos'
+  }
+
+  constructor(private http: HttpClient) {}
 
   public getUsers(): Observable<UsersResponse[]> {
-    return ObservableOf([]);
+    return this.http.get<UsersResponse[]>(this.urlObject.users);
   }
+
   public getAlbums(): Observable<AlbumsResponse[]> {
-    return ObservableOf([]);
+    return this.http.get<AlbumsResponse[]>(this.urlObject.albums);
   }
+
   public getPhotos(): Observable<PhotosResponse[]> {
-    return ObservableOf([]);
+    return this.http.get<PhotosResponse[]>(this.urlObject.photos);
   }
 }
