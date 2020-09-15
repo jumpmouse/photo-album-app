49504c4ceeb2f5e57c993b003a4a0215238be670
Milos <milosbr@gmail.com>
2020-09-15 13:09:54 +0200

    separate store and data transform logic

--- src/app/core/store.service.ts
+++ src/app/core/store.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { LoginInputData, UserInfo } from './core.interfaces';
+import { Albums, Photos, UserInfo } from './core.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StoreService {
   private userInfo$: BehaviorSubject<UserInfo> = new BehaviorSubject(null);
+  private albums$: BehaviorSubject<Albums> = new BehaviorSubject(null);
+  private photos$: BehaviorSubject<Photos> = new BehaviorSubject(null);
 
   constructor() {}
 
@@ -14,10 +16,26 @@ export class StoreService {
     return this.userInfo$.asObservable();
   }
 
-  public setUserInfo(inputData: LoginInputData): boolean {
-    const lastName = inputData.fullName.split(' ').pop();
-    const newUserInfo = { ...inputData, lastName};
-    this.userInfo$.next(newUserInfo);
+  public setUserInfo(userInfo: UserInfo): boolean {
+    this.userInfo$.next(userInfo);
+    return true;
+  }
+
+  public get albums(): Observable<Albums> {
+    return this.albums$.asObservable();
+  }
+
+  public setAlbums(albums: Albums): boolean {
+    this.albums$.next(albums);
+    return true;
+  }
+
+  public get photos(): Observable<Photos> {
+    return this.photos$.asObservable();
+  }
+
+  public setPhotos(photos: Photos): boolean {
+    this.photos$.next(photos);
     return true;
   }
 }
