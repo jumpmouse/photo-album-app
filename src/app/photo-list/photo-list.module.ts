e27a38d8cc8f1ecdc2a753faed69bf6c0ac9ba4c
Milos <milosbr@gmail.com>
2020-09-16 06:58:07 +0200

    update photos module

--- src/app/photo-list/photo-list.module.ts
+++ src/app/photo-list/photo-list.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { PhotoListRoutingModule } from './photo-list-routing.module';
 import { PhotoListComponent } from './photo-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [PhotoListComponent],
-  imports: [CommonModule, PhotoListRoutingModule]
+  imports: [PhotoListRoutingModule, SharedModule]
 })
 export class PhotoListModule {}
