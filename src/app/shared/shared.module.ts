10399c219f84d8815a6defb7f1bad83bcbcdaa5d
Milos <milosbr@gmail.com>
2020-09-16 03:17:38 +0200

    Add spinner component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,38 +1,52 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { ListComponent } from './UI/list/list.component';
 import { ListItemComponent } from './UI/list-item/list-item.component';
 import { ImageModalComponent } from './UI/image-modal/image-modal.component';
 import { DeleteDialogComponent } from './UI/delete-dialog/delete-dialog.component';
 import { ListSearchComponent } from './UI/list-search/list-search.component';
 import { ListSwitchViewComponent } from './UI/list-switch-view/list-switch-view.component';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SpinnerComponent } from './UI/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    ListComponent,
     ListItemComponent,
-    ImageModalComponent,
-    DeleteDialogComponent,
-    ListSearchComponent,
-    ListSwitchViewComponent
-  ],
-  imports: [CommonModule, ReactiveFormsModule, MatButtonModule, MatInputModule],
-  exports: [
     ListComponent,
-    ListItemComponent,
     ImageModalComponent,
     DeleteDialogComponent,
     ListSearchComponent,
     ListSwitchViewComponent,
+    SpinnerComponent
+  ],
+  imports: [
     CommonModule,
     ReactiveFormsModule,
     MatButtonModule,
-    MatInputModule
+    MatIconModule,
+    MatInputModule,
+    MatProgressSpinnerModule
+  ],
+  exports: [
+    ListItemComponent,
+    ListComponent,
+    ImageModalComponent,
+    DeleteDialogComponent,
+    ListSearchComponent,
+    ListSwitchViewComponent,
+    SpinnerComponent,
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatIconModule
   ]
 })
 export class SharedModule {}
